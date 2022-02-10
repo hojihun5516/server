13d6a511309828c08196fa48f4a796bf7ae1299e
hojihun5516 <hojihun5516@daum.net>
2022-02-10 17:42:09 +0900

    feat: Connect typeorm config
    issue #2-0

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,42 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
+// import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+// import { AuthModule } from './auth/auth.module';
+import { OwnerService } from './owner/owner.service';
+import { OwnerModule } from './owner/owner.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    // UsersModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.ENV_PATH,
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: async (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('MYSQL_URL'),
+        autoLoadEntities: true,
+        port: configService.get<number>('MYSQL_PORT'),
+        username: configService.get<string>('MYSQL_USERNAME'), //'root',
+        password: configService.get<string>('MYSQL_PASSWORD'), //'test',
+        database: configService.get<string>('MYSQL_DATABASE'),
+
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true, // true,
+        // ssl:
+        //   configService.get<string>('NODE_ENV') === 'production'
+        //     ? { rejectUnauthorized: false }
+        //     : false,
+      }),
+      inject: [ConfigService],
+    }),
+    OwnerModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, OwnerService],
 })
 export class AppModule {}
