85b973da34bc772dc9f7b5c57dec90f69adeeab1
hojihun5516 <hojihun5516@daum.net>
2022-02-25 17:22:15 +0900

    feat: Add interceptor and exception handler
    resolve #11

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
+import { SuccessInterceptor } from './common/interceptors/success.interceptor';
+import { HttpExceptionFilter } from './common/exceptions/http-exception.filter';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { setupSwagger } from './util/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalInterceptors(new SuccessInterceptor());
   setupSwagger(app);
   const PORT = process.env.PORT;
   await app.listen(PORT);
